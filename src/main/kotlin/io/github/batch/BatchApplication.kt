be3537cfcd760add29e2fdd3c52ab44dbccf23fd
ones1kk <ones1k95@gmail.com>
2025-12-02 23:44:52 +0900

    init

---
+++ src/main/kotlin/io/github/batch/BatchApplication.kt
@@ -0,0 +1,11 @@
+package io.github.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<BatchApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/batch/BatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
